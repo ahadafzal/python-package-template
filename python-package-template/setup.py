22ba96589e608cdaff0b7c50a2b550785e29a228
a.khan-at-456758482806 <a.khan@value3-advisory.com>
2020-05-18 16:11:19 +0530

    done

---
+++ python-package-template/setup.py
@@ -0,0 +1,23 @@
+"""Setup for the chocobo package."""
+
+import setuptools
+
+
+with open('README.md') as f:
+    README = f.read()
+
+setuptools.setup(
+    author="Ahad Afzal",
+    author_email="ahadbutafzal@gmail.com",
+    name='python-package-template',
+    license="MIT",
+    description='Whate ereve',
+    version='v0.0.1',
+    long_description=README,
+    url='https://github.com/ahadafzal/python-package-template',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires=[], # module dependencies
+    classifiers=[
+    ],
+)
